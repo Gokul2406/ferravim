5b5e3097eea9770405d0ba7b5e0c7c0a936235d8
Gokul2406 <gokulpbharathan89@gmail.com>
2021-07-30 15:51:43 +0530

    Packer setup and basic defaults

---
+++ lua/general.lua
@@ -0,0 +1,10 @@
+local o = vim.o
+
+o.swapfile=false
+o.relativenumber=true
+o.nu=true
+o.smartindent=true
+o.backup=false
+
+vim.g.mapleader = ' '
+
---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+vim.cmd [[packadd packer.nvim]]
+
+
+return require('packer').startup(function()
+
+	use 'wbthomason/packer.nvim'
+
+	use 'itchyny/lightline.vim'
+end)
